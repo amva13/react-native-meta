40b064af3b78cb3433f3a3abe40df40300e222e2
Alejandro Velez <alex@alexv24.com>
2023-04-15 19:14:05 -0600

    flatlist

--- little-lemon-app/App.js
+++ little-lemon-app/App.js
@@ -2,6 +2,7 @@ import { View, StyleSheet } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import Footer from './components/Footer';
 import WelcomeSreen from './WelcomeSreen';
+import MenuItems from './components/MenuItems';
 
 export default function App() {
   return (
@@ -12,6 +13,9 @@ export default function App() {
           <LittleLemonHeader />
           <View>
           < WelcomeSreen />
+        </View>
+        <View style={styles.items}>
+          <MenuItems />
         </View>
         </View>
         <View style={styles.footer}>
@@ -31,4 +35,8 @@ export default function App() {
     backgroundColor: '#333333',
     paddingBottom:10
   },
+  items: {
+    // padding:20,
+    flex:0.7,
+  }
  });
